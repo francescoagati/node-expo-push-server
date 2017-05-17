5501936c1962cf7a5c4f1b822077dec96f85c2c0
maticaputti <matias.caputti@gmail.com>
2017-05-17 16:08:36 -0300

    Include custom notif endpoint

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var Expo = require('exponent-server-sdk');
 
 // Create a new Expo SDK client
 let expo = new Expo();
 
 var app = express();
-var delayPushNotification = 3000;
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// set port
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', (request, response) => {
@@ -16,6 +23,11 @@ app.get('/', (request, response) => {
         title: '😄 Rmotr push notifications server',
         url: 'https://rmotr.com/',
         endpoints: [
+          {
+            url: '/notification',
+            method: 'POST',
+            description: 'Send push notification. Configured by the body object received { token, title, description, delay }.'
+          },
           {
             url: '/welcome/:token',
             method: 'POST',
@@ -35,6 +47,23 @@ app.get('/', (request, response) => {
       })
     }
   });
+});
+
+app.post('/notification', (request, response) => {
+  const { token, title, description, delay } = request.body;
+
+  let isPushToken = Expo.isExponentPushToken(token);
+
+  if (isPushToken) {
+    sendPush(token, title, description, response, delay);
+  } else {
+    response.json({
+        icon: '❌',
+        message: 'Your token is invalid',
+        token: token,
+        status: 'error'
+    });
+  }
 });
 
 app.post('/welcome/:token', (request, response) => {
@@ -91,21 +120,25 @@ app.post('/sendyo/:token/:from', (request, response) => {
   }
 });
 
-var sendPush = (token, message, description, response) => {
+var sendPush = (token, title, description, response, delay) => {
+  var delayPushNotification = delay || 0;
+
   setTimeout(() => {
     expo.sendPushNotificationAsync({
       // The push token for the app user you want to send the notification to
       to: token,
       sound: 'default',
-      body: message,
+      title: title || 'Push notification title',
+      body: description || 'Push notification description',
       data: {
-        response: message
+        title,
+        description
       },
     })
     .then((res) => {
       response.json({
             icon: '✅',
-            message: message,
+            message: title,
             description: description,
             token: token,
             status: 'sent',
@@ -114,7 +147,7 @@ var sendPush = (token, message, description, response) => {
     }, err => {
       response.json({
           icon: '❌',
-          message: message,
+          message: title,
           description: description,
           token: token,
           status: 'error'
